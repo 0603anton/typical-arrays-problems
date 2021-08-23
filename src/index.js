227ba428491af76833994605a328c58546f8fd59
0603Anton <zorgzb@gmail.com>
2021-08-23 08:15:57 +0700

    init: task done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    } else {
+        return Math.min.apply(null, array);
+    }
 
-exports.min = function min (array) {
-  return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    } else {
+        return Math.max.apply(null, array);
+    }
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    } else {
+        let sum = 0;
+        let lenght = array.length;
+        for (let elem of array) {
+            sum += elem;
+        }
+        return sum / lenght;
+    }
+};
